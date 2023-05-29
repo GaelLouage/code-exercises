9c4d63a96283bdd8ed81eb412ac57793167e63a9
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-05-29 10:48:21 +0200

    break camel case

---
+++ Code Exercises/Exercise6Kyu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Exercises
+{
+    internal  class Exercise6Kyu
+    {
+        public  StringBuilder BreakCamelCase(string str)
+        {
+            // complete the function
+            var builder = new StringBuilder();
+            foreach (var c in str)
+            {
+                if (char.IsUpper(c))
+                {
+                    builder.Append(" ");
+                }
+                builder.Append(c);
+            }
+            return builder;
+        }
+        public static string BreakCamelCaseLinq(string str) => string.Concat(str.Select(x => char.IsUpper(x) ? " " + x : x.ToString()));
+    }
+}
---
+++ Code Exercises/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Code_Exercises;
+
+Exercise6Kyu sixKyu = new Exercise6Kyu();
+Console.WriteLine("Hello, World!");
+Console.WriteLine(sixKyu.BreakCamelCase("testString"));
