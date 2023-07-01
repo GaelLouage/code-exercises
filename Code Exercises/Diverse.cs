4f8fc81e12024069d0b9446594de0ed4904723b4
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-07-01 19:22:58 +0200

    Paperwork

--- Code Exercises/Diverse.cs
+++ Code Exercises/Diverse.cs
@@ -393,6 +393,12 @@ namespace Code_Exercises
             }
             return sb.ToString();
         }
+
+        /*Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+        Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.*/
+         public static int Paperwork(int n, int m) =>
+            n < 0 || m < 0 ? 0 : n * m;
+
     }
 }
 
