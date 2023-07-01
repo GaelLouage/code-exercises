441793d15ea0a554bd3eaea2afd15230abf69b11
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-07-01 19:29:39 +0200

    GetNumbersWithRemainder0

---
+++ Code Exercises/LinqExrcises.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Exercises
+{
+    internal class LinqExrcises
+    {
+        /*Write a program in C# Sharp to show how the three parts of a query operation execute.
+        Expected Output:
+        The numbers which produce the remainder 0 after divided by 2 are :
+        0 2 4 6 8*/
+
+    }
+}
