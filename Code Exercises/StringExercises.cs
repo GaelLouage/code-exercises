1d65f523bef1ca33968ec646cc715e8484bd4b14
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-07-03 13:17:57 +0200

    CountWordsInString

---
+++ Code Exercises/StringExercises.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Exercises
+{
+    internal class StringExercises
+    {
+        /*count words in string*/
+        public static int CountWordsInString(string words) =>
+            words.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+}
