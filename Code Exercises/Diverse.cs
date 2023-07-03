ac3f08808774abfae7fd7e70ac521721d1c672ec
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-07-03 21:25:59 +0200

    GetNumericValue

--- Code Exercises/Diverse.cs
+++ Code Exercises/Diverse.cs
@@ -429,6 +429,15 @@ namespace Code_Exercises
             }
             return true;
         }
+
+        /*Convert number to reversed array of digits
+        Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+        
+        Example(Input => Output):
+        35231 => [1,3,2,5,3]
+        0 => [0]*/
+        public static long[] Digitize(long n) =>
+        n.ToString().Reverse().Select(x => (long)char.GetNumericValue(x)).ToArray();
     }
    
 }
