e25b9b61524046e8abf9518b2fafcf6bd2fdf407
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-07-04 18:19:23 +0200

    FirstNaturalNumbers

---
+++ Code Exercises/RecursionExercises.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Exercises
+{
+    internal class RecursionExercises
+    {
+        /* Write a program in C# Sharp to print the first n natural numbers using recursion.
+        Test Data :
+        How many numbers to print : 10
+        Expected Output :
+        1 2 3 4 5 6 7 8 9 10*/
+        public static void FirstNaturalNumbers(int n)
+        {
+            if (n == 1)
+            {
+                Console.Write(n);
+            }
+            else
+            {
+                FirstNaturalNumbers(n - 1);
+                Console.Write(" " + n);
+            }
+        }
+    }
+}
