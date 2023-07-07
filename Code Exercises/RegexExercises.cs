b72fb39c5fb4f6f596a231ddfa46e99821c3515e
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-07-07 13:34:31 +0200

    IsMatchingPhoneNumer

--- Code Exercises/RegexExercises.cs
+++ Code Exercises/RegexExercises.cs
@@ -18,7 +18,34 @@ namespace Code_Exercises
         */
         public static bool IsHexCode(string hexCode) =>
                Regex.IsMatch(hexCode, @"[#][0-9A-Fa-f]{6}\b");
-        
+
+        /*
+         Matching Phone Numbers
+         Write a regular expression pattern to match phone numbers in the following format:
+         
+         The phone number should consist of 10 digits.
+         The first digit cannot be zero.
+         The phone number can be in any format, including hyphens, spaces, or parentheses, as long as it consists of exactly 10 digits.
+         Your task is to write a regex pattern that matches phone numbers based on the above criteria.
+         
+         Example matches:
+         
+         1234567890
+         (123) 456-7890
+         123 456 7890
+         Example non-matches:
+         
+         12345 (too few digits)
+         0123456789 (first digit is zero)
+         (123)-456-7890 (extra characters)
+         In this pattern:
+
+        ^ asserts the start of the string.
+        [1-9] matches any digit from 1 to 9, ensuring that the first digit is not zero.
+        [0-9]{9} matches any 9 digits from 0 to 9.
+        $ asserts the end of the string.*/
+        public static bool IsMatchingPhoneNumer(string phoneNumber) => Regex.IsMatch(phoneNumber, @"^[1-9][0-9]{9}$");
+
 
     }
 }
