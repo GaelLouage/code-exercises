36fccc965252bbdfbce5a12e8b3fdb456627be39
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-07-07 13:14:09 +0200

    CompareStringWithoutLibrary

--- Code Exercises/StringExercises.cs
+++ Code Exercises/StringExercises.cs
@@ -11,5 +11,27 @@ namespace Code_Exercises
         /*count words in string*/
         public static int CountWordsInString(string words) =>
             words.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+        /*Write a program in C# Sharp to compare two strings without using a string library functions.
+        Test Data :
+        Input the 1st string : This is first string
+        Input the 2nd string : This is first string*/
+        public static bool CompareStringWithoutLibrary(string one, string two)
+        {
+            if (one.Length != two.Length) return false;
+            for (int i = 0; i < one.Length; i++)
+            {
+                if (one[i] != two[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CompareStringy(string one, string two)
+        {
+            return String.Compare(one, two) == 0;
+        }
     }
 }
