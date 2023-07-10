e8fc2b52714876e1fabc307044723c0e1065d152
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-07-10 11:01:03 +0200

    SquareNumbers

--- Code Exercises/LinqExrcises.cs
+++ Code Exercises/LinqExrcises.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,5 +19,15 @@ namespace Code_Exercises
         // find numbers in range
         public static List<int> NumbersInRange(List<int> numbers, int rangeStart, int rangeEnd) =>
               numbers.Where(x => x >= rangeStart && x <= rangeEnd).ToList();
+
+        /*Write a program in C# Sharp to find the number of an array and the square of each number.
+        Expected Output :
+        { Number = 9, SqrNo = 81 }
+        { Number = 8, SqrNo = 64 }
+        { Number = 6, SqrNo = 36 }
+        { Number = 5, SqrNo = 25 }
+        */
+        public static IEnumerable SquareNumbers(int[] array) =>
+             array.Select(x => new { Number = x, SqrNo = Math.Pow(x, 2) }).ToArray();
     }
 }
