bbc31f2897b790fc508a05b6ee2ace179cf3bece
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-08-10 15:18:58 +0200

    Remove_char

--- Code Exercises/Diverse.cs
+++ Code Exercises/Diverse.cs
@@ -886,6 +886,14 @@ namespace Code_Exercises
 
             return sb.ToString();
         }
+        /*It's pretty straightforward. Your goal is to create a 
+         * function that removes the first and last characters of a string. You're given one parameter, 
+         * the original string. You don't have to worry with strings with less than two characters.*/
+        public static string Remove_char(string s)
+        {
+            // Your Code
+            return s.Substring(1, s.Length - 2);
+        }
     }
 }
 
