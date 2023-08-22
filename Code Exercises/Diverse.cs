1f69d014f3e23aee50949c0dc2fd4cdf6006e6dd
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-08-22 09:08:47 +0200

    CalculateTip

--- Code Exercises/Diverse.cs
+++ Code Exercises/Diverse.cs
@@ -968,6 +968,38 @@ namespace Code_Exercises
             }
             return true;
         }
+        /*Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+       
+       You need to consider the following ratings:
+       
+       Terrible: tip 0%
+       Poor: tip 5%
+       Good: tip 10%
+       Great: tip 15%
+       Excellent: tip 20%
+       The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+       
+       "Rating not recognised" in Javascript, Python and Ruby...
+       ...or null in Java
+       ...or -1 in C#
+       Because you're a nice person, you always round up the tip, regardless of the service.*/
+        public static int CalculateTip(double amount, string rating)
+        {
+            var ratingLower = rating.ToLower();
+            var tipsDictionary = new Dictionary<string, int>()
+                                {
+                                  {"terrible",0},
+                                  {"poor",5},
+                                  {"good",10},
+                                  {"great",15},
+                                  {"excellent",20},
+                                };
+            if (tipsDictionary.ContainsKey(ratingLower))
+            {
+                return (int)(Math.Ceiling(amount * tipsDictionary[ratingLower] / 100));
+            }
+            return -1;
+        }
     }
 }
 
