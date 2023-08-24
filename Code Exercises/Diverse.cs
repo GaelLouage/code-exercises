ca080a6031fee326f3e8aa53d4c57414e66e2e5d
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-08-24 20:51:36 +0200

    pseudo-encryption algorithm

--- Code Exercises/Diverse.cs
+++ Code Exercises/Diverse.cs
@@ -1000,6 +1000,79 @@ namespace Code_Exercises
             }
             return -1;
         }
+        public static string Encrypt(string text, int n)
+        {
+            if (string.IsNullOrEmpty(text) || n <= 0)
+            {
+                return text;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                var sbEven = new StringBuilder();
+                var sbOdd = new StringBuilder();
+
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        sbEven.Append(text[j]);
+                    }
+                    else
+                    {
+                        sbOdd.Append(text[j]);
+                    }
+                }
+
+                text = sbOdd.ToString() + sbEven.ToString();
+            }
+
+            return text;
+        }
+        /*Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+        
+        Examples:
+        
+        encrypt("012345", 1)  =>  "135024"
+        encrypt("012345", 2)  =>  "135024"  ->  "304152"
+        encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+        
+        encrypt("01234", 1)  =>  "13024"
+        encrypt("01234", 2)  =>  "13024"  ->  "32104"
+        encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+        Together with the encryption function, you should also implement a decryption function which reverses the process.
+        
+        If the string S is an empty value or the integer N is not positive, return the first argument without changes.*/
+        public static string Decrypt(string encryptedText, int n)
+        {
+            if (string.IsNullOrEmpty(encryptedText) || n <= 0)
+            {
+                return encryptedText;
+            }
+
+            int length = encryptedText.Length;
+            int halfLength = length / 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                var sb = new StringBuilder();
+
+                for (int j = 0; j < halfLength; j++)
+                {
+                    sb.Append(encryptedText[j + halfLength]);
+                    sb.Append(encryptedText[j]);
+                }
+
+                if (length % 2 != 0)
+                {
+                    sb.Append(encryptedText[length - 1]);
+                }
+
+                encryptedText = sb.ToString();
+            }
+
+            return encryptedText;
+        }
     }
 }
 
