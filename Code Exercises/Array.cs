b46910d943d93968fb233da89d866c031c3dee66
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-10-02 08:59:03 +0200

    FindMinAndMaxValue

--- Code Exercises/Array.cs
+++ Code Exercises/Array.cs
@@ -103,5 +103,31 @@ namespace Code_Exercises
         {
             return array.OrderBy(x => x);
         }
+        /*Write a C# Sharp program to find the maximum and minimum elements in an array.
+        Test Data :
+        Input the number of elements to be stored in the array :3
+        Input 3 elements in the array :
+        element - 0 : 45
+        element - 1 : 25
+        element - 2 : 21
+        Expected Output :
+        Maximum element is : 45
+        Minimum element is : 21*/
+        public static (int min, int max) FindMinAndMaxValue(int[] array)
+        {
+            if (array is null || array.Length == 0)
+            {
+                return (0, 0);
+            }
+            // cannot surpass those values as int
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            foreach (var num in array)
+            {
+                if (min > num) min = num;
+                if (max < num) max = num;
+            }
+            return (min, max);
+        }
     }
 }
